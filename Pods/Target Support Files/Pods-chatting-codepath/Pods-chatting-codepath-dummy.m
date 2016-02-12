e0b7fa433f5782b2df742cc2b492ff38d443e03c
Oranuch Tangdechavut <finalehorizon@gmail.com>
2016-02-11 19:58:24 -0800

    Milestone 1: Finished

---
+++ Pods/Target Support Files/Pods-chatting-codepath/Pods-chatting-codepath-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatting_codepath : NSObject
+@end
+@implementation PodsDummy_Pods_chatting_codepath
+@end
---
+++ Pods/Target Support Files/Pods-chatting-codepath/Pods-chatting-codepath-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_chatting_codepathVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_chatting_codepathVersionString[];
+
